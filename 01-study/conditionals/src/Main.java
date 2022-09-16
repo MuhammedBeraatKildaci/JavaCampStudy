82cf43bf6037a7ddc1401511082dc988876c57ec
MuhammedBeraatKildaci <muhammedberaatkildaci@gmail.com>
2022-09-16 18:50:04 +0300

    conditionals has been done.

---
+++ 01-study/conditionals/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int sayi = 24;
+        if (sayi < 20) {
+            System.out.println("sayi 20 den küçüktür");
+        } else if (sayi == 20) {
+            System.out.println("sayi 20 ye eşittir");
+        } else {
+            System.out.println("sayi 20 den büyüktür");
+        }
+    }
+}
