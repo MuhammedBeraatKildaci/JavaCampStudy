bff1cafa3fcc3108ca02672502c1b498ead8704b
MuhammedBeraatKildaci <muhammedberaatkildaci@gmail.com>
2022-09-16 19:17:19 +0300

    for loop has been done

---
+++ 01-study/loopDemo/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // for loop
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("for döngüsü bitti.");
+    }
+}
