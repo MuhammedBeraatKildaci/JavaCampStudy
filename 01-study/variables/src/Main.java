ec4b81e49c239406101d14216efdc7e2e4ae4b96
MuhammedBeraatKildaci <muhammedberaatkildaci@gmail.com>
2022-09-16 17:50:24 +0300

    variables has been done.

---
+++ 01-study/variables/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int ogrenciSayisi = 12;
+        String mesaj = "Öğrenci sayım :";
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println("Öğrenci sayım :" + ogrenciSayisi);
+        System.out.println("Öğrenci sayım :" + ogrenciSayisi);
+        System.out.println("Öğrenci sayım :" + ogrenciSayisi);
+    }
+}
