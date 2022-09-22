724a8bf92a93c471358d602725def2d594b5f21f
MuhammedBeraatKildaci <muhammedberaatkildaci@gmail.com>
2022-09-22 16:43:50 +0300

    inheritance has been done.

---
+++ 02-study/inheritance/src/EmployeeManager.java
@@ -0,0 +1,5 @@
+public class EmployeeManager extends PersonManager {
+    public void BestEmployee(Employee employee) {
+        System.out.println("Ayın elemanı getirildi.");
+    }
+}
---
+++ 02-study/inheritance/src/PersonManager.java
@@ -0,0 +1,9 @@
+public class PersonManager {
+    public void List() {
+        System.out.println("Listelendi");
+    }
+
+    public void Add(Customer customer) {
+        System.out.println("Eklendi");
+    }
+}
