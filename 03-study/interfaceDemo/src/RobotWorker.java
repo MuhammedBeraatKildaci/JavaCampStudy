6a6e91bb32180efad4fdbdc417ff1f84ec650fac
Muhammed Beraat Kildaci <muhammedberaatkildaci@gmail.com>
2023-02-25 20:25:53 +0300

    interface demo has been done

---
+++ 03-study/interfaceDemo/src/RobotWorker.java
@@ -0,0 +1,7 @@
+public class RobotWorker implements Workable{
+
+    @Override
+    public void work() {
+    }
+    
+}
---
+++ 03-study/interfaceDemo/src/Worker.java
@@ -0,0 +1,15 @@
+public class Worker implements Workable,Payable,Eatable{
+
+    @Override
+    public void eat() {
+    }
+
+    @Override
+    public void pay() {
+    }
+
+    @Override
+    public void work() {
+    }
+     
+}
