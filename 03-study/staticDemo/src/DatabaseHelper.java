50cc634c73bfd0fb4e0c40fca45571ccebe02d3a
Muhammed Beraat Kildaci <muhammedberaatkildaci@gmail.com>
2023-02-25 20:53:24 +0300

    static has been done

---
+++ 03-study/staticDemo/src/DatabaseHelper.java
@@ -0,0 +1,12 @@
+public class DatabaseHelper {
+    public static class Crud {
+        public static void add(){}
+        public static void list(){}
+        public static void update(){}
+        public static void delete(){}
+    }
+
+    public static class Connection {
+        public static void connect(){}
+    }
+}
